fc91afddbc6866b83bdd23b84ef79059babe9e48
shendanqiong <shendanqiong@bytedance.com>
2021-02-14 08:49:00 +0800

    feat: 整理文件

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/home/index.vue'
-
-
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -13,22 +11,14 @@ const routes: Array<RouteConfig> = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
     path: '/regis',
     name: 'Registration',
-    component: () => import('../views/registration/index.vue')
+    component: () => import('../views/Registration.vue')
   },
   {
     path: '/info',
     name: 'Info',
-    component: () => import('../views/info/index.vue')
+    component: () => import('../views/Info.vue')
   }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="home-wrapper">
+    <div class="video-wrapper">
+      <!-- <video src="../../assets/yourname.mp4" controls="controls" width="90%" poster="../../assets/zju.jpg"></video> -->
+    </div>
   </div>
 </template>
-
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+import { Component, Vue } from 'vue-property-decorator'
+@Component
+export default class Home extends Vue {
+  name = 'ss'
+}
 </script>
+<style lang="less">
+.home-wrapper {
+  .video-wrapper {
+    margin: 50px auto;
+    width: 100%;
+  }
+}
+</style>
