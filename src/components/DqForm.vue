0243ac41142f0472a47b2bd338e097a7bf653324
dandanDQ <841294760@qq.com>
2021-03-19 23:58:12 +0800

    fix: 抽取表单组件

---
+++ src/components/DqForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="dq-form-wrapper" :style="{'width': formConfig.width? formConfig.width+'px': 'auto'}">
+    <dq-form-item v-for="(value, key) in formConfig.items" :key="key" :itemConfig="value"></dq-form-item>
+  </div>
+</template>
+<script lang="ts">
+  import { Component, Vue, Prop } from 'vue-property-decorator'
+  import DqFormItem from '@/components/DqFormItem.vue'
+  @Component({
+    components: {
+      DqFormItem
+    }
+  })
+  export default class FormItem extends Vue{
+    @Prop({
+      type: Object,
+      default: () => {
+        return {}
+      }
+    }) formConfig!: object
+  }
+</script>
+<style lang="less">
+.dq-form-wrapper {
+  margin: auto;
+}
+</style>
---
+++ src/components/DqFormItem.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="dq-form-item-wrapper">
+    <!-- 表单的基本项 -->
+
+    <div class="label-part">
+      <!-- hint: 对该项填写内容的提示说明，只支持纯文本 -->
+      <div class="hint-wrapper">
+        <el-popover
+          v-if="itemConfig.hint"
+          placement="top"
+          title=""
+          width="200"
+          trigger="hover"
+          :content="itemConfig.hint">
+          <i class="el-icon-question icon-color" slot="reference"></i>
+        </el-popover>
+      </div>
+
+      <!-- label: 表单的标签 -->
+      <span class="label">{{ itemConfig.label }}</span>
+
+      <!-- required: 是否为必填项 -->
+      <span class="required" :class="{ 'red': itemConfig.required }"></span>
+    </div>
+
+    <div class="input-part">
+      <!-- type: 表单填写项类型 -->
+      <div class="input-wrapper">
+        <el-input
+          v-if="itemConfig.type === 'input'"
+          type="text"
+          :placeholder="itemConfig.placeholder"
+          v-model="itemConfig.value"
+          :class="{'error-input': itemConfig.error}"
+        />
+        <el-input
+          v-if="itemConfig.type === 'password'"
+          type="password"
+          :placeholder="itemConfig.placeholder"
+          v-model="itemConfig.value"
+          :class="{'error-input': itemConfig.error}"
+        />
+        <slot />
+      </div>
+      <div class="error-wrapper" v-if="itemConfig.error">{{itemConfig.error}}</div>
+    </div>
+
+  </div>
+</template>
+<script lang="ts">
+  import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
+  import { Debounce } from 'vue-debounce-decorator'
+  interface ItemConf {
+    hint?: string;
+    label: string;
+    required?: boolean;
+    value: any;
+    rules?: any;
+    [propName: string]: any;
+  }
+
+  @Component({
+  })
+  export default class FormItem extends Vue{
+
+  @Watch('itemConfig.value')
+  @Debounce(500)
+  onValueChange(newVal: any){
+    console.log(newVal)
+    this.itemConfig.rules && this.itemConfig.rules.forEach((rule: any)=>{
+      if(!rule.ok(this.itemConfig.value)){
+        this.itemConfig.error = rule.msg
+      }
+    })
+  }
+
+
+  @Prop({
+    type: Object,
+    default: () => {
+      return {
+        hint: '不应该呀',
+        label: '请配置 label',
+        required: true,
+        value: '',
+        rules: []
+      }
+    }
+  }) itemConfig!: ItemConf
+}
+</script>
+<style lang="less">
+.dq-form-item-wrapper {
+  display: flex;
+  flex-direction: row;
+  background-color: antiquewhite;
+  padding: 4px;
+  margin: 8px;
+  .label-part {
+    background-color: aquamarine;
+    flex: 3;
+    text-align: right;
+    .hint-wrapper {
+      display: inline-block;
+      width: 16px;
+      height: 16px;
+      margin: 0 8px;
+      .icon-color {
+        color: #2775b6;
+      }
+    }
+
+    .label {
+      font-size: 16px;
+
+    }
+
+    .required {
+      display: inline-block;
+      width: 6px;
+      height: 6px;
+      margin: 4px;
+      vertical-align: middle;
+      border-radius: 50%;
+      &.red {
+        background-color: red;
+      }
+    }
+  }
+
+  .input-part {
+    background-color: sandybrown;
+    flex: 7;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    .input-wrapper {
+      align-self: stretch;
+      .error-input {
+        input {
+          border-color: red;
+        }
+      }
+    }
+    .error-wrapper {
+      color: red;
+      font-size: 14px;
+    }
+  }
+
+  
+
+}
+</style>
